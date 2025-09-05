c8268755e625832bc4d4cbc991b357540f0e72d2
SamsShow <sakshamtyagi2008@gmal.com>
2025-09-05 17:28:43 +0530

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Saksham Tyagi on 05/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThreadsCloneTests/ThreadsCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneTests.swift
+//  ThreadsCloneTests
+//
+//  Created by Saksham Tyagi on 05/09/25.
+//
+
+import Testing
+@testable import ThreadsClone
+
+struct ThreadsCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
