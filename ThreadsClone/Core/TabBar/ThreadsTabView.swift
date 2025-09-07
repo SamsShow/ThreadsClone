ae4c92b895ef75d3f697c9b207b461470f57c285
SamsShow <sakshamtyagi2008@gmal.com>
2025-09-07 17:25:47 +0530

    Tab View

---
+++ ThreadsClone/Core/TabBar/ThreadsTabView.swift
@@ -0,0 +1,18 @@
+//
+//  ThreadsTabView.swift
+//  ThreadsClone
+//
+//  Created by Saksham Tyagi on 07/09/25.
+//
+
+import SwiftUI
+
+struct ThreadsTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThreadsTabView()
+}
