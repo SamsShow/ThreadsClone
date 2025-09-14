ca27ceb6ec9fd20bb9aff145b696bae30472f2d6
SamsShow <sakshamtyagi2008@gmal.com>
2025-09-14 20:39:58 +0530

    Firebase Steup

---
+++ ThreadsClone/Core/Authentication/ViewModel/RegistrationViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  RegestrationViewModel.swift
+//  ThreadsClone
+//
+//  Created by Saksham Tyagi on 14/09/25.
+//
+
+import Foundation
+
+class RegistrationViewModel: ObservableObject {
+    
+    @MainActor
+    func createUser() async throws {
+        print("Debug: Create user here..")
+    }
+}
